a49b40586b76e3b4012e7f1b0b07f6b5e7ddc82b
Charles Buczel <charles.buczel@gmail.com>
2017-10-10 22:47:17 +0100

    plusMinus

---
+++ algorithms/warmup/plusMinus.js
@@ -0,0 +1,25 @@
+const plusMinus = (array) => {
+    const size = array.length;
+    let totalPositive = 0;
+    let totalNegative = 0;
+    let totalZero = 0;
+
+    for (let value of array) {
+        value === 0 && totalZero++;
+        value > 0 && totalPositive++;
+        value < 0 && totalNegative++;
+    }
+
+    return [totalPositive / size, totalNegative / size, totalZero / size]
+};
+
+const main = () => {
+    const size = parseInt(readLine());
+    const array = readLine().split(' ').map(Number);
+
+    const result = plusMinus(array);
+
+    console.log(result[0]);
+    console.log(result[1]);
+    console.log(result[2]);
+};
