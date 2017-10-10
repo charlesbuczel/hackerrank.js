309073b0f821914e076d99536c27fe2652241eff
Charles Buczel <charles.buczel@gmail.com>
2017-10-10 22:07:39 +0100

    solveMeFirst

---
+++ algorithms/warmup/simpleArraySum.js
@@ -0,0 +1,17 @@
+const simpleArraySum = (size, array) => {
+    result = 0;
+
+    for (let index = 0; index < size; index++) {
+        result += array[index];
+    }
+
+    return result;
+}
+
+const main = () => {
+    let size = parseInt(readLine());
+    let array = readLine().split(' ').map(Number);
+
+    process.stdout.write("" + simpleArraySum(size, array) + "\n");
+
+}
---
+++ algorithms/warmup/solveMeFirst.js
@@ -0,0 +1,8 @@
+const solveMeFirst = (a, b) => a + b;
+
+const main = () => {
+    let a = parseInt(readLine());
+    let b = parseInt(readLine());;
+
+    console.log(solveMeFirst(a, b));
+}
