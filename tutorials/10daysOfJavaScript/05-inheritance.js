251ed927655b5f9ffc84d3b5a199b7b903157fbf
Charles Buczel <charles.buczel@gmail.com>
2018-07-29 15:26:44 +0100

    Inheritance

---
+++ tutorials/10daysOfJavaScript/05-inheritance.js
@@ -0,0 +1,15 @@
+/*
+ *  Write code that adds an 'area' method to the Rectangle class' prototype
+ */
+Rectangle.prototype.area = function () {
+  return this.w * this.h;
+};
+
+/*
+ * Create a Square class that inherits from Rectangle and implement its class constructor
+ */
+class Square extends Rectangle {
+  constructor(a) {
+    super(a, a);
+  }
+}
