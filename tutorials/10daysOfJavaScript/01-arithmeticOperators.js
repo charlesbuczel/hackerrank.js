d063fe1571688c5a8f0abdd1e90982e66dde4032
Charles Buczel <charles.buczel@gmail.com>
2018-07-29 01:27:41 +0100

    Arithmetic Operators

---
+++ tutorials/10daysOfJavaScript/01-arithmeticOperators.js
@@ -0,0 +1,19 @@
+/**
+*   Calculate the area of a rectangle.
+*
+*   length: The length of the rectangle.
+*   width: The width of the rectangle.
+*
+*	Return a number denoting the rectangle's area.
+**/
+const getArea = (length, width) => length * width;
+
+/**
+*   Calculate the perimeter of a rectangle.
+*
+*	length: The length of the rectangle.
+*   width: The width of the rectangle.
+*
+*	Return a number denoting the perimeter of a rectangle.
+**/
+const getPerimeter = (length, width) => 2 * (length + width);
