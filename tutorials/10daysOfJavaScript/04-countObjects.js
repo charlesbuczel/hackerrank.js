7f0c53745ae04c830f8d768432dfbf175117583c
Charles Buczel <charles.buczel@gmail.com>
2018-07-29 12:59:50 +0100

    Classes, Objects

---
+++ tutorials/10daysOfJavaScript/04-countObjects.js
@@ -0,0 +1,7 @@
+/*
+ * Return a count of the total number of objects 'o' satisfying o.x == o.y.
+ *
+ * Parameter(s):
+ * objects: an array of objects with integer properties 'x' and 'y'
+ */
+const getCount = (objects) => objects.filter(o => o.x === o.y).length;
