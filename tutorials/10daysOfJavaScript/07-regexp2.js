889de911898f4f3869222464cfc188b10c4f17d7
Charles Buczel <charles.buczel@gmail.com>
2018-07-30 02:06:48 +0100

    Regex 3

--- tutorials/10daysOfJavaScript/07-regexp2.js
+++ tutorials/10daysOfJavaScript/07-regexp2.js
@@ -1,14 +1,14 @@
 const regexVar = () => {
-    /*
-     * Declare a RegExp object variable named 're'
-     * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.',
-     * followed by one or more letters.
-     */
-     const re = /^(Mr|Mrs|Ms|Dr|Er)(\.)[a-z|A-z]*$/;
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match a string that starts with 'Mr.', 'Mrs.', 'Ms.', 'Dr.', or 'Er.',
+   * followed by one or more letters.
+   */
+   const re = /^(Mr|Mrs|Ms|Dr|Er)(\.)[a-z|A-z]*$/;
 
 
-    /*
-     * Do not remove the return statement
-     */
-    return re;
+  /*
+   * Do not remove the return statement
+   */
+  return re;
 }
---
+++ tutorials/10daysOfJavaScript/07-regexp3.js
@@ -0,0 +1,12 @@
+const regexVar = () => {
+  /*
+   * Declare a RegExp object variable named 're'
+   * It must match ALL occurrences of numbers in a string.
+   */
+  const re = /\d+/g;
+
+  /*
+   * Do not remove the return statement
+   */
+  return re;
+}
