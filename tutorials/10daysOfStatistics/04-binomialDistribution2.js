92080c1dac72adaf47f3ca2774b5b27278af49fa
Charles Buczel <charles.buczel@gmail.com>
2018-08-04 21:33:13 +0100

    Binomial Distribution II

---
+++ tutorials/10daysOfStatistics/04-binomialDistribution2.js
@@ -0,0 +1,58 @@
+//https://www.hackerrank.com/challenges/s10-binomial-distribution-2/problem
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+const readLine = () => inputString[currentLine++];
+
+process.stdin.on('data', (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on("end", () => {
+  inputString = inputString.replace(/\s*$/, '')
+      .split('\n')
+      .map(str => str.replace(/\s*$/, ''));
+
+  main();
+});
+
+const main = () => {
+  const an = readLine().split(' ');
+  const a = parseInt(an[0], 10);
+  const b = 100 - a;
+
+  const pa = a / (a + b);
+  const pb = b / (a + b);
+
+  const n = parseInt(an[1], 10);
+
+  const lowerLimitA = 0;
+  const upperLimitA = 2;
+  console.log(cumulativeProbability(lowerLimitA, upperLimitA, n, pa, pb).toFixed(3));
+
+  const lowerLimitB = 2;
+  const upperLimitB = 10;
+  console.log(cumulativeProbability(lowerLimitB, upperLimitB, n, pa, pb).toFixed(3));
+};
+
+const cumulativeProbability = (lowerLimit, upperLimit, n, pa, pb) => {
+  let probability = 0;
+
+  for (let x = lowerLimit; x <= upperLimit; x++) {
+    probability += binomialDistribution(n, x, pa, pb);
+  }
+
+  return probability;
+}
+
+const binomialDistribution = (n, x, pa, pb) => combinations(n, x) * Math.pow(pa, x) * Math.pow(pb, n - x);
+
+const combinations = (n, x) => factorial(n) / (factorial(x) * factorial(n - x));
+
+const factorialCache = [1, 1];
+const factorial = (n) => factorialCache[n] ? factorialCache[n] : factorialCache[n] = factorial(n-1) * n;
