72a7b78bf5f92ddc728287f3f8dd53fed84dc092
Charles Buczel <charles.buczel@gmail.com>
2018-08-14 23:48:51 +0100

    Binary Numbers

---
+++ tutorials/30daysOfCode/10-binaryNumbers.js
@@ -0,0 +1,29 @@
+//https://www.hackerrank.com/challenges/30-binary-numbers/problem
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+const readLine = () => inputString[currentLine++];
+
+process.stdin.on('data', (inputStdin) => {
+  inputString += inputStdin;
+});
+
+process.stdin.on('end', () => {
+  inputString = inputString
+    .replace(/\s*$/, '')
+    .split('\n')
+    .map(str => str.replace(/\s*$/, ''));
+
+  main();
+});
+
+const main = () => {
+  const n = parseInt(readLine(), 10);
+
+  
+};
